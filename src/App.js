9f89e02c013fc96dd172ba26f242f2260b92241d
AlexisRc <alexisrca12@outlook.com>
2023-10-29 20:53:36 -0700

    show-numbers-operators-on-screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
+import Button from './componentes/Button';
+import Screen from './componentes/Screen';
+import ClearButton from './componentes/ClearButton';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
+
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const addInput = value => {
+
+    setInput(input + value);
+
+  };
+
+  const result = () => {
+
+    setInput(evaluate(input));
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logo-container'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+        <div className='calculator-container'>
+          <Screen input={input}/>
+          <div className='line'>
+            <Button manejarClic={addInput}>1</Button>
+            <Button manejarClic={addInput}>2</Button>
+            <Button manejarClic={addInput}>3</Button>
+            <Button manejarClic={addInput}>+</Button>
+          </div>
+          <div className='line'>
+            <Button manejarClic={addInput}>4</Button>
+            <Button manejarClic={addInput}>5</Button>
+            <Button manejarClic={addInput}>6</Button>
+            <Button manejarClic={addInput}>-</Button>
+          </div>
+          <div className='line'>
+            <Button manejarClic={addInput}>7</Button>
+            <Button manejarClic={addInput}>8</Button>
+            <Button manejarClic={addInput}>9</Button>
+            <Button manejarClic={addInput}>*</Button>
+          </div>
+          <div className='line'>
+            <Button manejarClic={result}>=</Button>
+            <Button manejarClic={addInput}>0</Button>
+            <Button manejarClic={addInput}>.</Button>
+            <Button manejarClic={addInput}>/</Button>
+          </div>
+          <div className='line'></div>
+            <ClearButton manageClear={() => setInput('')}>
+              Clear
+            </ClearButton>
+        </div>
+      </div>
+      
+
     </div>
   );
 }
